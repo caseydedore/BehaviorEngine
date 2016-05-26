539d2c63eaa28c46c8f072eaa879966a68eaa016
caseydedore <caseydedore@gmail.com>
2016-05-25 22:25:15 -0700

    Added initial RepeaterInfinite tests.

--- BehaviorEngine/Nodes/Decorators/RepeaterInfinite.cs
+++ BehaviorEngine/Nodes/Decorators/RepeaterInfinite.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BehaviorEngine.Nodes.Decorators
-{
+namespace BehaviorEngine
+{ 
+
     public class RepeaterInfinite : ANodeDecorator
     {
         private bool ignoreChildStatus = false;
---
+++ BehaviorEngineTests/RepeaterInfiniteTests.cs
@@ -0,0 +1,53 @@
+﻿using BehaviorEngine;
+using BehaviorEngineTests.Nodes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BehaviorEngineTests
+{
+    [TestClass]
+    public class RepeaterInfiniteTests
+    {
+        [TestMethod]
+        public void RepeatWithChildSuccess()
+        {
+            RepeatWithChildReturn(NodeState.Successful);
+        }
+
+        [TestMethod]
+        public void RepeatWithChildFailure()
+        {
+            RepeatWithChildReturn(NodeState.Failure);
+        }
+
+        [TestMethod]
+        public void RepeatWithChildError()
+        {
+            RepeatWithChildReturn(NodeState.Error);
+        }
+
+        [TestMethod]
+        public void RepeatWithChildActive()
+        {
+            RepeatWithChildReturn(NodeState.Active);
+        }
+
+
+        private void RepeatWithChildReturn(NodeState childStatus)
+        {
+            var repeater = new RepeaterInfinite();
+            repeater.Child = new FixedResultNode(childStatus);
+            var status = NodeState.Error;
+            var count = 1;
+
+            repeater.Start();
+
+            for(; count <= 1000; count++)
+            {
+                status = repeater.Update();
+                if (status != NodeState.Active) break;
+            }
+
+            Assert.AreEqual(count, 1000);
+        }
+    }
+}
