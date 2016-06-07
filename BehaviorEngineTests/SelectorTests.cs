2dbc5a5b70bd16812bfdf1050706488b686ec75b
caseydedore <caseydedore@gmail.com>
2016-06-06 22:52:55 -0700

    Started Selector tests.

---
+++ BehaviorEngineTests/SelectorTests.cs
@@ -0,0 +1,17 @@
+﻿using BehaviorEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BehaviorEngineTests
+{
+    [TestClass]
+    public class SelectorTests
+    {
+        [TestMethod]
+        public void NoChildren()
+        {
+            var selector = new Selector();
+
+            Assert.Fail("NO TEST");
+        }
+    }
+}
