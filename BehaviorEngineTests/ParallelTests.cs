7492bc0a795b06138d971a678eeae434ae34b1b4
caseydedore <caseydedore@gmail.com>
2016-06-10 22:26:59 -0700

    Added Parallel tests class.

---
+++ BehaviorEngineTests/ParallelTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorEngineTests
+{
+    [TestClass]
+    public class ParallelTests
+    {
+        [TestMethod]
+        public void NoChildren()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void OneChild()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TwoChildren()
+        {
+            Assert.Fail();
+        }
+    }
+}
