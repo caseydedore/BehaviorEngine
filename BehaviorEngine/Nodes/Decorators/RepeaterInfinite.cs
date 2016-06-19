0db624e853b67358e84d16d954ed7b9d57ef21ae
caseydedore <caseydedore@gmail.com>
2016-06-19 11:49:26 -0700

    Project now compliant with .NET 3.5 after removing unused namespaces not available in 3.5.

--- BehaviorEngine/Nodes/Decorators/RepeaterInfinite.cs
+++ BehaviorEngine/Nodes/Decorators/RepeaterInfinite.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BehaviorEngine
+﻿namespace BehaviorEngine
 { 
 
     public class RepeaterInfinite : ANodeDecorator
