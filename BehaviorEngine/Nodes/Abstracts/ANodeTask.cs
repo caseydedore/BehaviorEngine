cf89db8931fe671a2214d7a7bc7813235291966b
caseydedore <caseydedore@gmail.com>
2016-06-19 15:35:42 -0700

    ANodeTask Update method is now virtual and the implementation of which is not required.

--- BehaviorEngine/Nodes/Abstracts/ANodeTask.cs
+++ BehaviorEngine/Nodes/Abstracts/ANodeTask.cs
@@ -5,21 +5,10 @@ namespace BehaviorEngine
 {
     public abstract class ANodeTask : INode
     {
-        private ABehaviorEngine Master { get; set; }
-
         public NodeState Status { get; protected set; }
 
 
-        public ANodeTask(ABehaviorEngine master)
-        {
-            //potentially obsolete: custom ctors could simply pass the dependencies they actually need (As in the prototype).
-            Master = master;
-        }
-
-        public void Update()
-        {
-            throw new NotImplementedException();
-        }
+        public virtual void Update(){}
 
         public abstract void Start();
 
