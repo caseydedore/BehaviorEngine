0736898a7388a3e797f3173ab79b1a7b7fda92fe
caseydedore <caseydedore@gmail.com>
2016-06-26 23:07:23 -0700

    Added Extender.
    The Extender may be used to extend a branch, taking the same status as it's child in all cases.

---
+++ BehaviorEngine/Nodes/Decorators/Extender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehaviorEngine.Nodes.Decorators
+{
+    public class Extender : ANodeDecorator
+    {
+        public override void Update()
+        {
+            Child.Update();
+            Status = Child.Status;
+        }
+    }
+}
