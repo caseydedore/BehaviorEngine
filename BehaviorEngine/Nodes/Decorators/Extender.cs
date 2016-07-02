8798d92f656c6aae9b9c6bf64af99e34a06925d7
caseydedore <caseydedore@gmail.com>
2016-07-02 15:57:01 -0700

    Fixed Extender namespace.

--- BehaviorEngine/Nodes/Decorators/Extender.cs
+++ BehaviorEngine/Nodes/Decorators/Extender.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BehaviorEngine.Nodes.Decorators
+namespace BehaviorEngine
 {
     public class Extender : ANodeDecorator
     {
