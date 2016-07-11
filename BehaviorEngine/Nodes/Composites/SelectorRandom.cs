b279867df3f845ddd6bdce7f7f72a135a425d722
caseydedore <caseydedore@gmail.com>
2016-07-10 22:45:15 -0700

    Added SelectorRandom, SelectorRandomTests stub.

---
+++ BehaviorEngine/Nodes/Composites/SelectorRandom.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace BehaviorEngine
+{
+    public class SelectorRandom : ANodeComposite
+    {
+        private int index = 0;
+        private int IndexMax { get; set; }
+        private Random Random { get; set; }
+
+
+        public override void Update()
+        {
+            if (Children.Count <= 0)
+            {
+                Status = NodeState.Error;
+                return;
+            }
+
+            if(index == -1)
+            {
+                index = Random.Next(IndexMax);
+            }
+
+            Children[index].Update();
+            Status = Children[index].Status;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            index = -1;
+            IndexMax = Children.Count - 1;
+        }
+
+        public override void End()
+        {
+            base.End();
+            index = -1;
+        }
+    }
+}
